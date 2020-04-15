4c4e17d7550b359196676bda327f7dba0234a6b6
Nicholas H.Tollervey <ntoll@ntoll.org>
2020-04-15 18:54:14 +0100

    Chrome tabs

--- mu/editor.js
+++ mu/editor.js
@@ -2,4 +2,5 @@ var editorInstance = ace.edit('container', {
     mode: "ace/mode/python",
     selectionStyle: "text"
 });
+// editorInstance.setTheme('ace/theme/dawn');
 editorInstance.setTheme('ace/theme/twilight');
