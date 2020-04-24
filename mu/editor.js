068abdebe13dff6f6af2296023d95ae2a246ecf4
Nicholas H.Tollervey <ntoll@ntoll.org>
2020-04-24 17:41:04 +0100

    CSS tidy ups. Needs work.

--- mu/editor.js
+++ mu/editor.js
@@ -1,6 +1,12 @@
-var editorInstance = ace.edit('container', {
+var editorInstance = ace.edit('code-hello', {
     mode: "ace/mode/python",
     selectionStyle: "text"
 });
 // editorInstance.setTheme('ace/theme/dawn');
 editorInstance.setTheme('ace/theme/twilight');
+var editorInstance2 = ace.edit('code-test', {
+    mode: "ace/mode/python",
+    selectionStyle: "text"
+});
+// editorInstance.setTheme('ace/theme/dawn');
+editorInstance2.setTheme('ace/theme/twilight');
